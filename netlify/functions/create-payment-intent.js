f1b49c761be79c4c0b63a80d73b7854e714b6fd8
nicholaspugsley <nkpcoding@gmail.com>
2024-03-18 02:47:45 -0400

    5

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -6,34 +6,18 @@ exports.handler = async (event) => {
     }
 
     const { items, customer } = JSON.parse(event.body);
-    const totalAmount = items.reduce((acc, item) => acc + (item.price * item.quantity), 0);
+    const totalAmount = items.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
     try {
-        // Preparing product details for metadata
-        const productDetails = items.map(item => `ID: ${item.id}, Price: ${item.price}, Quantity: ${item.quantity}`).join('; ');
-
         const paymentIntent = await stripe.paymentIntents.create({
             amount: totalAmount,
             currency: 'usd',
-            receipt_email: customer.email, // Directly use email for receipts
+            receipt_email: customer.email,
             metadata: {
-                fullName: customer.fullName, // Consider if you still need it in metadata since we'll use billing_details
+                fullName: customer.fullName,
                 phone: customer.phone,
-                productDetails // Including detailed product info for backend processing
+                productDetails: items.map(item => `ID: ${item.id}, Price: ${item.price}, Quantity: ${item.quantity}`).join('; ')
             },
-            // Using billing_details to include the name
-            payment_method_options: {
-                card: {
-                    request_three_d_secure: 'automatic'
-                }
-            },
-            payment_method_data: {
-                type: 'card',
-                billing_details: {
-                    name: customer.fullName // Include the name in billing details
-                    // Email is not directly included here, as it's set above with receipt_email
-                }
-            }
         });
 
         return {
